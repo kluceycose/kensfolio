c5cec50f19bd5864a82bacd919540bdef79dcd92
kluceycose <kluceycose@gmail.com>
2020-06-06 17:30:37 -0700

    Updates

---
+++ precache-manifest.f3b407b9144e3fe1b379a444cb3ac172.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692b120a692d2ca334bb2417691b3f5c",
+    "url": "/kensfolio/index.html"
+  },
+  {
+    "revision": "07d67551f71619e1d703",
+    "url": "/kensfolio/static/css/main.e7a9b9d7.chunk.css"
+  },
+  {
+    "revision": "c71d6498b909c4c5fea7",
+    "url": "/kensfolio/static/js/2.80e8ed81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kensfolio/static/js/2.80e8ed81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07d67551f71619e1d703",
+    "url": "/kensfolio/static/js/main.9ebcc249.chunk.js"
+  },
+  {
+    "revision": "0d6e0f66906a662865a0",
+    "url": "/kensfolio/static/js/runtime-main.d0f4c2b1.js"
+  },
+  {
+    "revision": "d6f76ac6cf8c837beadfdc6788ea5708",
+    "url": "/kensfolio/static/media/logo1.d6f76ac6.svg"
+  }
+]);
