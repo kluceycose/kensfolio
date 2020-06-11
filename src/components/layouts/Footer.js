089793fb42dde1b9f85bcbd28c704969f5c9a5f1
Ken Lucey-Cose <kluceycose@gmail.com>
2020-06-11 11:57:24 -0700

    Removed email from footer, added target=_blank to external footer links

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -12,18 +12,18 @@ function Footer(){
             <p>Bay Area, California</p>
         </div>
         <div className="footer-3 footer__item">
-            <h4>Email</h4>    
-            <p>kluceycose@gmail.com</p>
+            <h4>Contact Information</h4>    
+            <p>Please see my resume</p>
         </div>        
         <div className="footer-4 footer__item">
             <IconContext.Provider value={{size: "3em"}}>
-                <a href="https://www.linkedin.com/in/kluceycose" className="footer-icon-link">
+                <a href="https://www.linkedin.com/in/kluceycose" className="footer-icon-link" target="_blank">
                     <FaLinkedin className="footer-4__item"/>
                 </a>
-                <a href="https://www.github.com/kluceycose" className="footer-icon-link">
+                <a href="https://www.github.com/kluceycose" className="footer-icon-link" target="_blank">
                     <FaGithubSquare className="footer-4__item"/>
                 </a>
-                <a href="https://drive.google.com/file/d/1NN5HUtPPQOgaYK8hSA-526znr3yd29Xa/view?usp=sharing" className="footer-icon-link">
+                <a href="https://drive.google.com/file/d/1NN5HUtPPQOgaYK8hSA-526znr3yd29Xa/view?usp=sharing" className="footer-icon-link" target="_blank">
                     <FaFileAlt />
                 </a>
             </IconContext.Provider>
